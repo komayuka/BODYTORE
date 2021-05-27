5a8b7833ceea6ea902dcfcad2fcf82e971b13fc9
komayuka <komayuka115@gmail.com>
2021-05-27 01:58:57 +0000

    [update]レイアウト調整

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,6 @@ class Review < ApplicationRecord
   belongs_to :trainer
 
   validates :title, presence: true
-  validates :body, presence: true
+  validates :body, presence: true, length: { maximum: 50 }
 
 end
